20019a590e202764ff95aaaa3c39d0ba71961862
Aniol <aniolbarnadat@gmail.com>
2022-11-09 20:40:55 +0100

    Done basic raycast for player interaction, need to call hit obj interact

---
+++ Assets/Scripts/Player/PlayerInteract.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    [SerializeField] LayerMask layerMask;
+
+    private RaycastHit hit;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                Debug.Log("Did Hit");
+                DoInteraction(hit.transform.gameObject);
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+                Debug.Log("Did not Hit");
+            }
+        }
+    }
+
+    private void DoInteraction(GameObject interactedObject)
+    {
+        Debug.Log(interactedObject);
+        // Call interact logic
+    }
+}
